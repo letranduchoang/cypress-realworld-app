04860e6fa9de2bbdf0ab6458dcb2a1d0dbd4ef58
letranduchoang <40495320+letranduchoang@users.noreply.github.com>
2024-04-02 15:26:55 +0700

    update

--- cypress/tests/demo/cypress-studio.spec.ts
+++ cypress/tests/demo/cypress-studio.spec.ts
@@ -1,17 +1,35 @@
-import { User } from "models";
+// import { User } from "models";
 
-describe("Cypress Studio Demo", function () {
-  beforeEach(function () {
-    cy.task("db:seed");
+// describe("Cypress Studio Demo", function () {
+//   beforeEach(function () {
+//     cy.task("db:seed");
 
-    cy.database("find", "users").then((user: User) => {
-      cy.login(user.username, "s3cret", { rememberUser: true });
-    });
-  });
-  it("create new transaction", function () {
-    // Extend test with Cypress Studio
-  });
-  it("create new bank account", function () {
-    // Extend test with Cypress Studio
-  });
-});
+//     cy.database("find", "users").then((user: User) => {
+//       cy.login(user.username, "s3cret", { rememberUser: true });
+//     });
+//   });
+//   it("create new transaction", function () {
+//     // Extend test with Cypress Studio
+//   });
+//   it("create new bank account", function () {
+//     // Extend test with Cypress Studio
+//   });
+// });
+/// <reference types="Cypress" />
+const username = 'chiphoi'
+const password = '123456'
+
+describe('My First Test Suite', function () {
+
+  it('My FirstTest case', function () {
+
+    cy.visit("/")
+    cy.get('#username').type(username)
+    cy.get("#password").type(password)
+    cy.get('[data-test="signin-submit"]').click()
+    cy.get('')
+  })
+
+
+
+})
